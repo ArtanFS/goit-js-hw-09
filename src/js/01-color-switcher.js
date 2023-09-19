e504254e34f02cea196f47fe7a0dc0174e363051
ArtanFS <132677965+ArtanFS@users.noreply.github.com>
2023-09-19 23:54:18 +0300

    make task01 js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
 
+const body = document.querySelector('body');
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+let timerId = null;
+stopBtn.setAttribute('disabled', '');
+
+const clickStart = () => {
+  startBtn.setAttribute('disabled', '');
+  stopBtn.removeAttribute('disabled', '');
+  timerId = setInterval(() => {
+    const bgColor = getRandomHexColor();
+    body.style.backgroundColor = bgColor;
+  }, 1000);
+};
+
+const clickStop = () => {
+  startBtn.removeAttribute('disabled', '');
+  stopBtn.setAttribute('disabled', '');
+  clearInterval(timerId);
+};
+
+startBtn.addEventListener('click', clickStart);
+stopBtn.addEventListener('click', clickStop);
