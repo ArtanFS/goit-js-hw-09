36cf3a16b597afa81bf6e78e1576cd91fc6e0364
ArtanFS <132677965+ArtanFS@users.noreply.github.com>
2023-09-24 23:34:45 +0300

    begin make task03

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,36 @@
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+  return promise;
 }
+
+const refs = {
+  delay: document.querySelector('input[name=delay]'),
+  step: document.querySelector('input[name=step]'),
+  amount: document.querySelector('input[name=amount]'),
+  btn: document.querySelector('.form'),
+};
+
+refs.delay.setAttribute('min', 0);
+refs.step.setAttribute('min', 0);
+refs.amount.setAttribute('min', 0);
+
+refs.btn.addEventListener('submit', evt => {
+  evt.preventDefault();
+  createPromise(refs.amount, refs.delay)
+    .then(({ position, delay }) => {
+      console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(({ position, delay }) => {
+      console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+    });
+  refs.btn.reset();
+});
