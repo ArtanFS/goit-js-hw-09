88c5b5b239f3f9a6ac40d29f96632f3afa1b19ba
ArtanFS <132677965+ArtanFS@users.noreply.github.com>
2023-09-25 15:39:07 +0000

    Deploying to gh-pages from @ ArtanFS/goit-js-hw-09@8ad68c76f70f038ae99af8c88c2987cebc03a153 🚀

--- 01-color-switcher.cbb0ede4.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("body"),e=document.querySelector("[data-start]"),r=document.querySelector("[data-stop]");let d=null;r.setAttribute("disabled","");e.addEventListener("click",(()=>{e.setAttribute("disabled",""),r.removeAttribute("disabled",""),d=setInterval((()=>{const e=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`;t.style.backgroundColor=e}),1e3)})),r.addEventListener("click",(()=>{e.removeAttribute("disabled",""),r.setAttribute("disabled",""),clearInterval(d)}));
-//# sourceMappingURL=01-color-switcher.cbb0ede4.js.map
